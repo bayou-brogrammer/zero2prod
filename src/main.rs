9a1fddf80b466a9f9acfd864f59c37b2b8143201
Jacob LeCoq <lecoqjacob@gmail.com>
2023-07-04 20:20:46 -0500

    complete chapter 5

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::net::TcpListener;
 
-use secrecy::ExposeSecret;
 use sqlx::postgres::PgPoolOptions;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -14,8 +13,7 @@ async fn main() {
         zero2prod::configuration::get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(configuration.database.connection_string().expose_secret())
-        .expect("Failed to connect to Postgres.");
+        .connect_lazy_with(configuration.database.with_db());
 
     let listener = TcpListener::bind(configuration.application.address()).unwrap();
     run(listener, connection_pool).await.unwrap();
